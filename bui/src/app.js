d98bbca89d52903228982718fd4170e96ddb2ad8
Adam Bien <abien@adam-bien.com>
2020-06-17 19:31:34 +0200

    datepicker integrated

--- bui/src/app.js
+++ bui/src/app.js
@@ -1,13 +1,16 @@
 import { sayHello } from "./client.js";
+import "./DatePicker.js";
+
 class Message extends HTMLElement{
 
     constructor() { 
         super();
-        this.message = sayHello("JUG O.");
+        this.message = sayHello("JUG Ob.");
     }
 
     connectedCallback() { 
         this.innerHTML = `
+            <ui5-datepicker id="myDatepicker1"></ui5-datepicker>
             hello ${this.message}
         `;
     }
--- resources.md
+++ resources.md
@@ -1,6 +1,8 @@
 # Links and Resources
 
 https://docs.emmet.io/cheat-sheet/
+https://sap.github.io/ui5-webcomponents/
+https://rollupjs.org/guide/en/
 
 https://airhacks.live
 https://airhacks.tv
