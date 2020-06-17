b72776bb9d0a0ad7357cc0c8ccd16ed3efd7dab2
Adam Bien <abien@adam-bien.com>
2020-06-17 19:04:50 +0200

    lithtml is not a framework

--- bui/src/app.js
+++ bui/src/app.js
@@ -1,4 +1,14 @@
 console.log("Hello Web Standards 2");
 
+const html = (a, b, c) => { 
+    return "etwas zurueck " + a + b + c
+}
+
+const helloJug = (message) => { 
+    return html`JUG Oberpfalz says: ${message}`;
+    
+}
+
+console.log(helloJug("hello"));
 
 
